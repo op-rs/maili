2b635e6ebce884c5398977d0fbc00d8e76d10958
Emilia Hane <elsaemiliaevahane@gmail.com>
2025-01-09 14:22:19 +0100

    Update doc headers

--- crates/rpc-types-engine/src/lib.rs
+++ crates/rpc-types-engine/src/lib.rs
@@ -1,7 +1,7 @@
 #![doc = include_str!("../README.md")]
 #![doc(
-    html_logo_url = "https://raw.githubusercontent.com/alloy-rs/core/main/assets/alloy.jpg",
-    html_favicon_url = "https://raw.githubusercontent.com/alloy-rs/core/main/assets/favicon.ico"
+    html_logo_url = "https://raw.githubusercontent.com/op-rs/maili/main/assets/square.png",
+    html_favicon_url = "https://raw.githubusercontent.com/op-rs/maili/main/assets/favicon.ico"
 )]
 #![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
 #![cfg_attr(not(test), warn(unused_crate_dependencies))]

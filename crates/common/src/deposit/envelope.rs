07d6bb88aa15ab828a63f3c26ee21ef60940af9b
Emilia Hane <elsaemiliaevahane@gmail.com>
2025-01-14 19:42:52 +0100

    Fix docs

--- crates/common/src/deposit/envelope.rs
+++ crates/common/src/deposit/envelope.rs
@@ -4,11 +4,11 @@ use alloy_consensus::Sealed;
 
 use crate::TxDeposit;
 
-/// Transaction envelope that encompasses a [`DepositTransaction`].
+/// Transaction envelope that encompasses a [`TxDeposit`].
 pub trait DepositTxEnvelope {
     /// Returns `true` if the transaction is a deposit transaction.
     fn is_deposit(&self) -> bool;
 
-    /// Returns [`DepositTransaction`] if transaction is a deposit transaction.
+    /// Returns [`TxDeposit`] if transaction is a deposit transaction.
     fn as_deposit(&self) -> Option<&Sealed<TxDeposit>>;
 }

0f159afc5e17fc26db75029536595af5df4c0aa6
Emilia Hane <elsaemiliaevahane@gmail.com>
2025-01-14 19:16:57 +0100

    Replace DepositTxEnvelope with concrete type

--- crates/common/src/deposit/envelope.rs
+++ crates/common/src/deposit/envelope.rs
@@ -2,16 +2,13 @@
 
 use alloy_consensus::Sealed;
 
-use crate::DepositTransaction;
+use crate::TxDeposit;
 
 /// Transaction envelope that encompasses a [`DepositTransaction`].
 pub trait DepositTxEnvelope {
-    /// Deposit transaction.
-    type DepositTx: DepositTransaction;
-
     /// Returns `true` if the transaction is a deposit transaction.
     fn is_deposit(&self) -> bool;
 
     /// Returns [`DepositTransaction`] if transaction is a deposit transaction.
-    fn as_deposit(&self) -> Option<&Sealed<Self::DepositTx>>;
+    fn as_deposit(&self) -> Option<&Sealed<TxDeposit>>;
 }

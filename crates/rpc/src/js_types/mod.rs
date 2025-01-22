1590117c2807a49fb89218bb399e525e422515d6
refcell <abigger87@gmail.com>
2025-01-22 13:03:43 -0500

    fix(rpc): cleans up the maili-rpc crate

--- crates/rpc/src/js_types/mod.rs
+++
@@ -1,9 +0,0 @@
-mod config;
-pub use config::{Genesis, RollupConfig, SystemConfig};
-
-
-
-mod superchain;
-pub use superchain::{
-    ProtocolVersion, ProtocolVersionError, ProtocolVersionFormatV0, SuperchainSignal,
-};

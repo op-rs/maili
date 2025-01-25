67d0d3816f8e2696074d6bff616b126693ba5d76
sergerad <serge.radinovich@immutable.com>
2025-01-25 20:57:19 +1300

    Add validation impl with timeout

--- crates/rpc/src/traits.rs
+++ crates/rpc/src/traits.rs
@@ -3,43 +3,70 @@ use core::time::Duration;
 use crate::api::SupervisorApiClient;
 use alloy_primitives::Log;
 use alloy_sol_types::SolEvent;
-use maili_interop::ExecutingMessage;
+use maili_interop::ExecutingMessage as InteropMessage;
 use maili_interop::CROSS_L2_INBOX_ADDRESS;
+use maili_protocol::ExecutingMessage as ProtocolMessage;
+use tokio::time::error::Elapsed;
 
-/// TODO docs.
+pub use maili_protocol::SafetyLevel;
+
+/// Failures occurring during validation of [ExecutingMessage]s.
 #[derive(thiserror::Error, Debug)]
-pub enum ExecutingMessageValidatorError {
-    /// TODO docs.
-    #[error("TODO")]
-    Todo,
-    /// TODO docs.
-    #[error("TODO: {0}")]
-    AlloySolTypesError(#[from] alloy_sol_types::Error),
+pub enum Error {
+    /// Failure during Supervisor's validation of [ExecutingMessage]s.
+    #[error("Supervisor determined messages are invalid: {0}")]
+    SupervisorValidationError(#[from] jsonrpsee_core::ClientError),
+
+    /// Message validation against the Supervisor took longer than allowed.
+    #[error("Message validation timed out: {0}")]
+    ValidationTimeout(#[from] Elapsed),
 }
 
 /// TODO docs.
 #[cfg(all(feature = "jsonrpsee", feature = "client"))]
 pub trait ExecutingMessageValidator {
-    /// TODO docs.
-    type SupervisorClient: SupervisorApiClient;
+    /// RPC client to Supervisor instance used for [ExecutingMessage] validation.
+    type SupervisorClient: SupervisorApiClient + Sync;
+
+    /// Default duration that message validation is not allowed to exceed.
+    const DEFAULT_TIMEOUT: Duration;
 
     /// Extracts [ExecutingMessage]s from the [Log] if there are any.
-    fn parse_messages(logs: &[Log]) -> impl Iterator<Item = ExecutingMessage> {
+    fn parse_messages(logs: &[Log]) -> impl Iterator<Item = InteropMessage> {
         logs.iter().filter_map(|log| {
             // TODO: Are there any error variants here that we want to consider
             // as failures rather than filtering out with `ok()`?
             (log.address == CROSS_L2_INBOX_ADDRESS && log.topics().len() == 2)
-                .then(|| ExecutingMessage::decode_log_data(&log.data, true).ok())
+                .then(|| InteropMessage::decode_log_data(&log.data, true).ok())
                 .flatten()
         })
     }
 
-    /// TODO docs.
-    fn validate_messages(
-        supervisor: Self::SupervisorClient,
-        messages: &[ExecutingMessage],
+    /// Validates a list of [ExecutingMessages] against a Supervisor instance
+    /// accessed through the [SupervisorClient].
+    async fn validate_messages(
+        supervisor: &Self::SupervisorClient,
+        messages: &[ProtocolMessage],
+        safety: SafetyLevel,
         timeout: Option<Duration>,
-    ) -> Result<(), ExecutingMessageValidatorError> {
-        todo!()
+    ) -> Result<(), Error> {
+        // Set timeout duration based on input if provided.
+        let timeout = match timeout {
+            Some(t) => t,
+            None => Self::DEFAULT_TIMEOUT,
+        };
+
+        // Construct the future to validate all messages using supervisor.
+        // TODO: SupervisorApiClient::check_messages() should take &[ExecutingMessage]?
+        let messages = messages.to_vec();
+        let check = async {
+            supervisor
+                .check_messages(messages, safety)
+                .await
+                .map_err(|e| Error::SupervisorValidationError(e))
+        };
+
+        // Await the validation future with timeout.
+        tokio::time::timeout(timeout, check).await.map_err(Error::ValidationTimeout)?
     }
 }

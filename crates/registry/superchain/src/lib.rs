3d1877110feb23d738f1a8eb50a6834d364cc01b
refcell <abigger87@gmail.com>
2025-02-05 16:32:32 -0500

    fix(superchain): Bad Superchain Types (#178)
    ### Description
    
    Fixes the bad serialization of types in `maili-superchain`.

--- crates/registry/superchain/src/lib.rs
+++ crates/registry/superchain/src/lib.rs
@@ -11,7 +11,7 @@ extern crate alloc;
 
 use alloc::{string::String, vec::Vec};
 use alloy_primitives::Address;
-use maili_genesis::ChainConfig;
+use maili_genesis::{ChainConfig, HardForkConfiguration};
 
 /// A superchain configuration.
 #[derive(Debug, Clone, Default, Eq, PartialEq, serde::Serialize, serde::Deserialize)]
@@ -26,30 +26,32 @@ pub struct Superchain {
 
 /// A superchain configuration file format
 #[derive(Debug, Clone, Default, Hash, Eq, PartialEq, serde::Serialize, serde::Deserialize)]
-// #[serde(rename_all = "PascalCase")]
 pub struct SuperchainConfig {
     /// Superchain name (e.g. "Mainnet")
     pub name: String,
     /// Superchain L1 anchor information
     pub l1: SuperchainL1Info,
+    /// Default hardforks timestamps.
+    pub hardforks: HardForkConfiguration,
     /// Optional addresses for the superchain-wide default protocol versions contract.
+    #[serde(alias = "protocolVersionsAddr")]
     pub protocol_versions_addr: Option<Address>,
     /// Optional address for the superchain-wide default superchain config contract.
+    #[serde(alias = "superchainConfigAddr")]
     pub superchain_config_addr: Option<Address>,
     /// The op contracts manager proxy address.
-    #[serde(rename = "OPContractsManagerProxyAddr")]
+    #[serde(alias = "OPContractsManagerProxyAddr")]
     pub op_contracts_manager_proxy_addr: Option<Address>,
 }
 
 /// Superchain L1 anchor information
 #[derive(Debug, Clone, Default, Hash, Eq, PartialEq, serde::Serialize, serde::Deserialize)]
-// #[serde(rename_all = "PascalCase")]
 pub struct SuperchainL1Info {
     /// L1 chain ID
-    // #[serde(rename = "ChainID")]
+    #[serde(alias = "chainId")]
     pub chain_id: u64,
     /// L1 chain public RPC endpoint
-    // #[serde(rename = "PublicRPC")]
+    #[serde(alias = "publicRPC")]
     pub public_rpc: String,
     /// L1 chain explorer RPC endpoint
     pub explorer: String,
@@ -70,6 +72,29 @@ mod tests {
 
     #[test]
     fn test_superchain_serde() {
+        let raw: &str = r#"
+        {
+            "name": "Mainnet",
+            "config": {
+                "name": "Mainnet",
+                "l1": {
+                    "chainId": 10,
+                    "publicRPC": "https://mainnet.rpc",
+                    "explorer": "https://mainnet.explorer"
+                },
+                "hardforks": {
+                    "canyon_time": 1699981200,
+                    "delta_time": 1703203200,
+                    "ecotone_time": 1708534800,
+                    "fjord_time": 1716998400,
+                    "granite_time": 1723478400,
+                    "holocene_time": 1732633200
+                }
+            },
+            "chains": []
+        }
+        "#;
+
         let superchain = Superchain {
             name: "Mainnet".to_string(),
             config: SuperchainConfig {
@@ -79,16 +104,24 @@ mod tests {
                     public_rpc: "https://mainnet.rpc".to_string(),
                     explorer: "https://mainnet.explorer".to_string(),
                 },
+                hardforks: HardForkConfiguration {
+                    canyon_time: Some(1699981200),
+                    delta_time: Some(1703203200),
+                    ecotone_time: Some(1708534800),
+                    fjord_time: Some(1716998400),
+                    granite_time: Some(1723478400),
+                    holocene_time: Some(1732633200),
+                    isthmus_time: None,
+                    interop_time: None,
+                },
                 protocol_versions_addr: None,
                 superchain_config_addr: None,
                 op_contracts_manager_proxy_addr: None,
             },
-            chains: vec![ChainConfig::default()],
+            chains: vec![],
         };
 
-        let serialized = serde_json::to_string(&superchain).unwrap();
-        let deserialized: Superchain = serde_json::from_str(&serialized).unwrap();
-
+        let deserialized: Superchain = serde_json::from_str(raw).unwrap();
         assert_eq!(superchain, deserialized);
     }
 }

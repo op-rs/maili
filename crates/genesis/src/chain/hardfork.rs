214c953ea5e241fcfccc07aae20cab05ebc89733
yohkaz <yoh.kzl@gmail.com>
2025-02-17 01:58:59 +0200

    HardForkConfiguration deserialization fails upon unknown field

--- crates/genesis/src/chain/hardfork.rs
+++ crates/genesis/src/chain/hardfork.rs
@@ -6,6 +6,7 @@
 #[derive(Debug, Copy, Clone, Default, Hash, Eq, PartialEq)]
 #[cfg_attr(feature = "arbitrary", derive(arbitrary::Arbitrary))]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[serde(deny_unknown_fields)]
 pub struct HardForkConfiguration {
     /// Canyon hardfork activation time
     pub canyon_time: Option<u64>,
@@ -23,4 +24,51 @@ pub struct HardForkConfiguration {
     pub isthmus_time: Option<u64>,
     /// Interop hardfork activation time
     pub interop_time: Option<u64>,
+}
+
+#[cfg(test)]
+#[cfg(feature = "serde")]
+mod tests {
+    use super::*;
+    use toml;
+
+    #[test]
+    fn test_hardforks_deserialize() {
+        let raw: &str = r#"
+        canyon_time =  1699981200 # Tue 14 Nov 2023 17:00:00 UTC
+        delta_time =   1703203200 # Fri 22 Dec 2023 00:00:00 UTC
+        ecotone_time = 1708534800 # Wed 21 Feb 2024 17:00:00 UTC
+        fjord_time =   1716998400 # Wed 29 May 2024 16:00:00 UTC
+        granite_time = 1723478400 # Mon Aug 12 16:00:00 UTC 2024
+        holocene_time = 1732633200 # Tue Nov 26 15:00:00 UTC 2024
+        "#;
+
+        let hardforks = HardForkConfiguration {
+            canyon_time: Some(1699981200),
+            delta_time: Some(1703203200),
+            ecotone_time: Some(1708534800),
+            fjord_time: Some(1716998400),
+            granite_time: Some(1723478400),
+            holocene_time: Some(1732633200),
+            isthmus_time: None,
+            interop_time: None,
+        };
+
+        let deserialized: HardForkConfiguration = toml::from_str(raw).unwrap();
+        assert_eq!(hardforks, deserialized);
+    }
+
+    #[test]
+    fn test_hardforks_deserialize_new_field_fail() {
+        let raw: &str = r#"
+        canyon_time =  1699981200 # Tue 14 Nov 2023 17:00:00 UTC
+        delta_time =   1703203200 # Fri 22 Dec 2023 00:00:00 UTC
+        ecotone_time = 1708534800 # Wed 21 Feb 2024 17:00:00 UTC
+        fjord_time =   1716998400 # Wed 29 May 2024 16:00:00 UTC
+        granite_time = 1723478400 # Mon Aug 12 16:00:00 UTC 2024
+        holocene_time = 1732633200 # Tue Nov 26 15:00:00 UTC 2024
+        new_field_time = 1732633200 # Tue Nov 26 15:00:00 UTC 2024
+        "#;
+        toml::from_str::<HardForkConfiguration>(raw).unwrap_err();
+    }
 }
